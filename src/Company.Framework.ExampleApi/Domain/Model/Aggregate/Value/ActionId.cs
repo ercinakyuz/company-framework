ec83436ddedc93cf963b13ff91868dc7eb52df82
Erçin <b.ercin.akyuz@gmail.com>
2023-09-14 04:42:44 +0300

    Refactor

--- src/Company.Framework.ExampleApi/Domain/Model/Aggregate/Value/ActionId.cs
+++ src/Company.Framework.ExampleApi/Domain/Model/Aggregate/Value/ActionId.cs
@@ -3,7 +3,7 @@ using Company.Framework.Core.Id.Implementations;
 
 namespace Company.Framework.ExampleApi.Domain.Model.Aggregate.Value
 {
-    public record ActionId(Guid Value) : IdOfGuid<ActionId>(Value), IId<ActionId, Guid>
+    public record ActionId(Guid Value) : IdOfGuid<ActionId>(Value)
     {
     }
 }

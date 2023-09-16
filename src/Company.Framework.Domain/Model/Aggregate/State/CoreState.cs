5757a73c14ace74bc5032f346388f8ca4d362bb9
Erçin <b.ercin.akyuz@gmail.com>
2023-09-16 23:05:11 +0300

    Refactor

--- src/Company.Framework.Domain/Model/Aggregate/State/CoreState.cs
+++ src/Company.Framework.Domain/Model/Aggregate/State/CoreState.cs
@@ -2,7 +2,7 @@
 
 public abstract record CoreState<TState>(string Value) where TState : CoreState<TState>
 {
-    public static readonly TState Loaded = (TState)Activator.CreateInstance(typeof(TState), "Loaded")!;
+    public static readonly TState Loaded = From("Loaded")!;
 
     public static TState? From(string? value)
     {
